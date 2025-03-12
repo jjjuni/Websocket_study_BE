5d46428716beda770baceca46ee891c8e6fa8466
이준희 <0210leejun@gmail.com>
2025-03-12 10:14:03 +0900

    :tada: 프로젝트 생성

---
+++ src/test/java/com/websocket/websocketStudy/WebsocketStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.websocket.websocketStudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebsocketStudyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
